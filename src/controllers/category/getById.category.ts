0b0458c8ef02d647c01f973da8f074e945c92ce1
Chemirik-mohamed <contact@chemirikmohamed.com>
2025-05-09 18:06:04 +0200

    fix: - Updated getById.category.ts

--- src/controllers/category/getById.category.ts
+++ src/controllers/category/getById.category.ts
@@ -1,15 +1,15 @@
 import type { Context } from "hono";
 import { prisma } from "../../lib/prisma";
 import {
-  GetCategoryByIdSchema,
-  type GetCategoryByIdSchemaInput,
   paramIdSchema,
   type paramIdSchemaInput,
 } from "../../schemas/category.schema";
 
 export const getCategoryById = async (c: Context) => {
   const dataId = c.req.param("id");
-  const validatedDataId: paramIdSchemaInput = paramIdSchema.parse(dataId);
+  const validatedDataId: paramIdSchemaInput = paramIdSchema.parse({
+    id: dataId,
+  });
   const category = await prisma.category.findUnique({
     where: { id: validatedDataId.id },
   });
--- src/controllers/comment/create.comment.ts
+++ src/controllers/comment/create.comment.ts
@@ -1,8 +1,17 @@
-// import { Prisma } from "@prisma/client";
-// import type { Context } from "hono";
-// import { CreateComment } from "schemas/commentaire.schema";
+import { Prisma } from "@prisma/client";
+import type { Context } from "hono";
+import { prisma } from "lib/prisma";
+import { CreateComment } from "schemas/commentaire.schema";
 
-// export const createComment = async (c: Context): Promise<Response> => {
-//   const body = c.req.json();
-//   const data = CreateComment.parse(body);
-// };
+export const createComment = async (c: Context): Promise<Response> => {
+  const body = c.req.json();
+  const data = CreateComment.parse(body);
+  const comment = await prisma.comment.create({
+    data: {
+      content: data.content,
+      user: { connect: { id: data.usersId } },
+      recipe: { connect: { id: data.recipesId } },
+    },
+  });
+  return c.json(comment);
+};
